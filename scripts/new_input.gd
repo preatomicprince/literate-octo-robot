76b259e2c50db770a1ce61d19b4492f399d6d35e
preatomicprince <tom.cafferata@gmail.com>
2024-11-16 03:29:35 +0000

    Streamlined input RPC calls to server

--- scripts/new_input.gd
+++ scripts/new_input.gd
@@ -13,47 +13,43 @@ enum EVENT_TYPE {
 	mouse_left,
 	mouse_right
 }
-func _input(event: InputEvent) -> void:
-	if not is_multiplayer_authority():
-		return
-	if event is InputEventMouseMotion:
-		mouse_pos = $".."/Map.local_to_map($".."/Map.get_local_mouse_position())# + $"..".camera[peer_id].offset)
-		rpc_id(1, "sync_mouse_pos", mouse_pos)
 	
 func _process(delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
 		
-	var event_type
+	var new_events = []
+	
+	mouse_pos = $".."/Map.local_to_map($".."/Map.get_local_mouse_position())# + $"..".camera[peer_id].offset)
+	rpc_id(1, "sync_mouse_pos", mouse_pos)
 	
 	if Input.is_action_pressed("key_left"):
-		event_type = EVENT_TYPE.key_left
-		rpc_id(1, "push_event_to_server", event_type)
+		new_events.append(EVENT_TYPE.key_left)
 		
 	if Input.is_action_pressed("key_right"):
-		event_type = EVENT_TYPE.key_right
-		rpc_id(1, "push_event_to_server", event_type)
+		new_events.append(EVENT_TYPE.key_right)
 		
 	if Input.is_action_pressed("key_up"):
-		event_type = EVENT_TYPE.key_up
-		rpc_id(1, "push_event_to_server", event_type)
+		new_events.append(EVENT_TYPE.key_up)
 		
 	if Input.is_action_pressed("key_down"):
-		event_type = EVENT_TYPE.key_down
-		rpc_id(1, "push_event_to_server", event_type)
+		new_events.append(EVENT_TYPE.key_down)
 	
 	if Input.is_action_just_pressed("key_e"):
 		print(mouse_pos)
-		event_type = EVENT_TYPE.key_e
-		rpc_id(1, "push_event_to_server", event_type)
+		new_events.append(EVENT_TYPE.key_e)
+		
+	if not new_events.is_empty():
+		rpc_id(1, "push_event_to_server", new_events)
 		
 	if Input.is_action_pressed("key_esc"):
 		get_tree().quit() 
 
 # Channel 2 is reserved for transfering input to server
 @rpc("authority", "unreliable", "call_remote", 2)
-func push_event_to_server(event):
-	event_queue.push_back(event)
+func push_event_to_server(auth_events):
+	for event in auth_events:
+		event_queue.push_back(event)
 	
 @rpc("authority", "unreliable", "call_remote", 2)
 func sync_mouse_pos(auth_mouse_pos):
